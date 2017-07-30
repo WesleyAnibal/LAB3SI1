c69b36c0ea92d969c00fe1a0e7e45a55d4c4f4e4
wesleyanibal <wesley.anibal@ccc.ufcg.edu.br>
2017-07-30 14:53:24 -0300

    g

--- src/main/java/br/com/Lab3Si1/services/ControllerUsuario.java
+++ src/main/java/br/com/Lab3Si1/services/ControllerUsuario.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping("/api")
 public class ControllerUsuario {
 
 	@Autowired
